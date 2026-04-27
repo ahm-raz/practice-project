fd0a749513340a01ae003b2102c97a7eca9ea45c
Maheen987Hafiza <maheenashraf6588@gmail.com>
2026-04-27 09:21:34 -0700

    made my route

---
+++ backend/routes/my.route.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+const router = express.Router();
+
+
+router.get('/',(req,res)=>{
+    res.send("Hello My router");
+})
+
+export default router;
+
+
+// actually mujhe galtii lagii thi, comaand /"
+//no issue sir 
+// real command  " git clone link " hai. ok
+
+// all ok , now this new file is ceated, and we made seveal changes in code, now we re gonna push code. ok
+// hmay main folder say code ko push krna hota ha, so move back 
